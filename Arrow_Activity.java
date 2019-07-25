7de8038a17a2fa3d05fba2897b87c2df467b0fbe
tdio123 <45208468+tdio123@users.noreply.github.com>
2019-07-25 23:23:55 +0200

    Update Arrow_Activity.java

--- Arrow_Activity.java
+++ Arrow_Activity.java
@@ -1,9 +1,6 @@
-package ikm.wang.vogitninverse;
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
 import ikm.wang.vogitninverse.View.ArrowView;
 
 public class Arrow_Activity extends AppCompatActivity {
